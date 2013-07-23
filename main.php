6032d6d96e00384d5c19cf9d95e3937e7940bd4a
David Hayes <dhayes@gmail.com>
2013-07-22 18:25:04 -0600

    Initial commit

---
+++ main.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: Only One Category
+Plugin URI: http://pressupinc.com
+Description: Makes categories as radio buttons
+Author: Press Up
+Version: 0.1.0
+Author URI: http://pressupinc.com
+*/ 
+
+if( strstr($_SERVER['REQUEST_URI'], 'wp-admin/post-new.php') 
+	|| strstr($_SERVER['REQUEST_URI'], 'wp-admin/post.php') 
+	|| strstr($_SERVER['REQUEST_URI'], 'wp-admin/edit.php') )
+{
+  ob_start('ooc_one_category_only');
+}
+
+
+function ooc_one_category_only($content) {
+	return ooc_swap_out_checkboxes($content);
+}
+
+
+function ooc_swap_out_checkboxes($content) {
+	$content = str_replace('type="checkbox" name="post_category', 'type="radio" name="post_category', $content);
+
+	foreach (get_all_category_ids() as $i) { 
+		$content = str_replace('id="in-popular-category-'.$i.'" type="checkbox"', 'id="in-popular-category-'.$i.'" type="radio"', $content);
+	}
+
+	return $content;
+}
